1928ad1f91179f8b6bb73ef6f5ecd1bbef077131
tester <tester@tester.com>
2025-12-22 11:50:29 +0900

    랜덤버튼까지

--- stories/admin.py
+++ stories/admin.py
@@ -3,6 +3,5 @@ from .models import Episode
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'station', 'episode_num', 'subtitle', 'last_viewed_at')
-    list_filter = ('station',)
-    search_fields = ('station__station_name', 'subtitle')
+    list_display = ('station', 'title', 'image')
+    fields = ('station', 'title', 'image')
--- subway/admin.py
+++ subway/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Station, Line, StationLine
 
-# Register your models here.
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('station_name', 'station_code', 'is_enabled', 'image')
+    list_filter = ('is_enabled', 'stationline__line__line_name')  
+    search_fields = ('station_name', 'station_code')
+
+admin.site.register(Station, StationAdmin)
+admin.site.register(Line)
+admin.site.register(StationLine)
