f1c0fb33fdf558a480e8a582e661b76df59c40ac
MinjiKnG <kangminji5017@gmail.com>
2025-12-23 10:13:08 +0900

    Revert "Merge pull request #17 from MEITHER-HIST/feat/myPage"
    This reverts commit 0034e18854a055481e5def62292d702c5b06a2be, reversing
    changes made to f3ff41b4796c36f66138f5b66c0c7ec021ca21c4.

--- stories/admin.py
+++ stories/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Episode, EpisodeImage
-
-class EpisodeImageInline(admin.TabularInline):
-    model = EpisodeImage
-    extra = 0
-
-@admin.register(Episode)
-class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'station', 'episode_num', 'title')
-    inlines = [EpisodeImageInline]
-=======
 from .models import Episode
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
     list_display = ('station', 'title', 'image')
     fields = ('station', 'title', 'image')
->>>>>>> 0d6b3f83263c69e43d272063447f5061c2759c13
