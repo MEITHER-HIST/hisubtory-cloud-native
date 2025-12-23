02a8d41cd77533d559539d3cf08d7c5c04493567
MinjiKnG <kangminji5017@gmail.com>
2025-12-23 10:13:08 +0900

    Revert "Merge pull request #17 from MEITHER-HIST/feat/myPage"
    This reverts commit 669612b0710a5690cc77c103878b076421287830, reversing
    changes made to a12a8bb75e09d32522eeb6d0a7ac6aa3291178cf.

--- project/urls.py
+++ project/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')), 
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
-    path('api/stories/', include('stories.urls')),
-    path('api/library/', include('library.urls')),
-    path('api/accounts/', include('accounts.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stories/models.py
+++ stories/models.py
@@ -1,29 +1,4 @@
 from django.db import models
-<<<<<<< HEAD
-from django.utils import timezone # 추가
-
-class Episode(models.Model):
-    station = models.ForeignKey('subway.Station', on_delete=models.CASCADE)
-    episode_num = models.IntegerField(default=1)
-    title = models.CharField(max_length=200, default='제목 없음') # 기본값 추가
-    subtitle = models.CharField(max_length=200, blank=True, null=True, default='')
-    history_summary = models.TextField()
-    # default=timezone.now 를 추가하여 빈 데이터 문제 해결
-    last_viewed_at = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        unique_together = ('station', 'episode_num')
-
-    def __str__(self):
-        return f"[{self.station.name}] {self.title}"
-
-class EpisodeImage(models.Model):
-    # related_name='images'를 통해 episode.images.all()로 접근 가능합니다.
-    episode = models.ForeignKey(Episode, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='episode_images/')
-    caption = models.CharField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
-=======
 from subway.models import Station
 
 def episode_upload_to(instance, filename):
@@ -36,4 +11,3 @@ class Episode(models.Model):
 
     def __str__(self):
         return f"{self.station.station_name} - {self.title}"
->>>>>>> 0d6b3f83263c69e43d272063447f5061c2759c13
