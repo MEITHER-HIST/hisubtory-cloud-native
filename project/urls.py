e64f180a9dcd1dead86475c1c499e9ab0a75931a
tester <tester@tester.com>
2025-12-24 13:04:20 +0900

    .

--- project/urls.py
+++ project/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
     path('health/', health),
+    path('stories/', include('stories.urls')),
 ]
