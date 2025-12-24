478ff2701cea5bdf170c44af3953b84427c770da
MinjiKnG <kangminji5017@gmail.com>
2025-12-24 10:49:49 +0900

    stop tracking pycache, fix deploy&infra

--- project/urls.py
+++ project/urls.py
@@ -19,10 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("ok", content_type="text/plain")
 
 urlpatterns = [
     path("health/", views.health),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
+    path('health/', health),
 ]
