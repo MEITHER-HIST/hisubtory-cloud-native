73e94ac1bfc4970903a7e442fb8f01575676682e
MinjiKnG <kangminji5017@gmail.com>
2025-12-24 17:18:22 +0900

    update stories_models for S3

--- stories/views.py
+++ stories/views.py
@@ -58,3 +58,37 @@ def toggle_bookmark(request, episode_id):
         bookmark.delete()  # 이미 북마크 되어있으면 취소
     return redirect('episode_detail', episode_id=episode_id)
 
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
+# from .models import Episode
+# from .serializers import EpisodeSerializer
+# from subway.models import Station
+# import random
+
+# @api_view(['GET'])
+# def station_stories(request, station_id):
+#     station = get_object_or_404(Station, id=station_id)
+#     episodes = Episode.objects.filter(station=station)
+
+#     if episodes.exists():
+#         episode = random.choice(episodes)
+#         serializer = EpisodeSerializer(episode)
+#         return Response(serializer.data)
+#     else:
+#         return Response({"message": "해당 역의 스토리가 없습니다."}, status=404)
+
+from rest_framework import generics
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Cut
+from .serializers import CutSerializer
+
+class EpisodeCutListCreateView(generics.ListCreateAPIView):
+    serializer_class = CutSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self):
+        return Cut.objects.filter(episode_id=self.kwargs["episode_id"]).order_by("cut_order")
+
+    def perform_create(self, serializer):
+        serializer.save(episode_id=self.kwargs["episode_id"])
