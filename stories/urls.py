0d0a337237e3d538b6252a7d06fffc19e8453ca7
MinjiKnG <kangminji5017@gmail.com>
2025-12-24 17:18:22 +0900

    update stories_models for S3

--- stories/urls.py
+++ stories/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from stories.views import EpisodeCutListCreateView
 from . import views
 
 urlpatterns = [
     # 브라우저 클릭 시 HTML 렌더링
     path('episode/<int:episode_id>/', views.episode_detail_view, name='episode_detail'),
-
-    # 북마크 토글 (로그인 필요)
-    path('episode/<int:episode_id>/bookmark/', views.toggle_bookmark, name='toggle_bookmark'),
+    # pages, stories API
+    path('api/v1/lines/', include('pages.urls_api')),
+    path('api/v1/stories/', include('stories.urls_api')),
+    path("api/v1/episodes/<int:episode_id>/cuts/", EpisodeCutListCreateView.as_view(), name="episode_cuts"),
 ]
