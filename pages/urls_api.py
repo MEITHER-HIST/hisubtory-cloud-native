bbfeb099a0b7369c9a2807ec9c94699ac829eb2d
MinjiKnG <kangminji5017@gmail.com>
2025-12-30 13:06:57 +0900

    로그인/비로그인 구분

--- pages/urls_api.py
+++ pages/urls_api.py
@@ -1,12 +1,13 @@
+# pages/urls_api.py
 from django.urls import path
 from . import views_api
 
 urlpatterns = [
-    # 메인 페이지 데이터
-    path('v1/main', views_api.main_api_view, name='main_api'),
-
-    # 마이페이지 데이터 (로그인 필요)
-    path('v1/mypage', views_api.mypage_api_view, name='mypage_api'),
+    path("v1/main/", views_api.main_api_view, name="pages_main"),
+    path("v1/episode/pick/", views_api.pick_episode_api_view, name="pages_episode_pick"),
+    path("v1/episode/random/", views_api.random_episode_api_view, name="pages_episode_random"),
+    path("v1/mypage/", views_api.mypage_api_view, name="pages_mypage"),
+    path("v1/auth/mock-login/", views_api.mock_login_api_view),
+    path("v1/auth/logout/", views_api.logout_api_view),
+    path("v1/auth/me/", views_api.me_api_view),
 ]
-
- 
