0a49298a8a17484a790bd3547cac74fd853b8328
joinmin0114-lab <joinmin0114@gmail.com>
2026-01-02 20:05:06 +0900

    feat: 기능 확인 완료

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import User, OAuthAccount
 
-# Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # 어드민 목록에 보일 필드 설정
+    list_display = ('username', 'email', 'is_staff', 'created_at')
+    # 상세 페이지에서 수정 가능한 필드 세트
+    fieldsets = UserAdmin.fieldsets + (
+        ('추가 정보', {'fields': ('created_at', 'updated_at')}),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(OAuthAccount)
--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Bookmark, UserViewedEpisode
 
-# Register your models here.
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'episode', 'created_at')
+    search_fields = ('user__username', 'episode__episode_title')
+    list_filter = ('created_at',)
+
+@admin.register(UserViewedEpisode)
+class UserViewedEpisodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'episode', 'viewed_at')
+    search_fields = ('user__username', 'episode__episode_title')
+    list_filter = ('viewed_at',)
