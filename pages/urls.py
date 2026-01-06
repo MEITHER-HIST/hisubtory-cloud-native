fa1fffb1f994dd00b353b2638c32e855569a166c
MinjiKnG <kangminji5017@gmail.com>
2026-01-06 15:52:41 +0900

    error fixed

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from . import views_api as views
 app_name = "pages"
 
 urlpatterns = [
-    path('v1/main/', views.main_view, name='main_api'),
+    path('v1/main/', views.main_api_view, name='main_api'),
     path("v1/episode/pick/", views.pick_episode_api_view, name="pick_episode_api"),
     path('v1/episode/random/', views.get_random_episode_api, name='random_episode_api'),
 ]
