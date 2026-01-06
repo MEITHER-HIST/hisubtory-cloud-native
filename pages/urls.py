8c5357c334eb6346b801a3d4347c0e04d49ed515
MinjiKnG <kangminji5017@gmail.com>
2026-01-06 15:49:32 +0900

    error fixed

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import views_api as views
+
+app_name = "pages"
 
 urlpatterns = [
-    # Main 화면 데이터 (역 목록 등)
     path('v1/main/', views.main_view, name='main_api'),
-    
-    # ✅ 리액트가 호출하는 바로 그 경로!
+    path("v1/episode/pick/", views.pick_episode_api_view, name="pick_episode_api"),
     path('v1/episode/random/', views.get_random_episode_api, name='random_episode_api'),
 ]
