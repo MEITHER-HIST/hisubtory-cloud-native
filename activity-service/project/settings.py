1c926646ef9d41695219e9611984a5c9a6445d91
Tester <tester@example.com>
2026-03-13 00:49:25 +0900

    Fix Security: Disable COOP header and loosen cookie security for HTTP environment to fix Admin login issues

--- activity-service/project/settings.py
+++ activity-service/project/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # 정적 파일 서빙용
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,11 +92,23 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# WhiteNoise 설정 최적화
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
+
+# --- 보안 및 쿠키 설정 완화 (HTTP 환경용) ---
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://hisubtory-alb-1990322498.ap-northeast-2.elb.amazonaws.com",
+    "http://localhost:5173",
+]
 
 # --- 핵심 세션 공유 설정 ---
 SESSION_COOKIE_NAME = 'hisubtory_sessionid'
@@ -113,9 +125,13 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
+        "KEY_PREFIX": ""
     }
 }
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+SESSION_COOKIE_PATH = '/'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -125,8 +141,5 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://hisubtory-alb-1990322498.ap-northeast-2.elb.amazonaws.com",
-]
-CSRF_TRUSTED_ORIGINS = [
     "http://hisubtory-alb-1990322498.ap-northeast-2.elb.amazonaws.com",
 ]
