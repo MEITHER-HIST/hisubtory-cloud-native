2498fdb6398f68cf56d7540778e48bfdebcc43a1
Tester <tester@example.com>
2026-03-14 01:36:45 +0900

    fix: FINAL FINAL FINAL NUCLEAR REBUILD V3 - Cleaned configs and hardcoded all settings

--- user-service/project/settings.py
+++ user-service/project/settings.py
@@ -4,16 +4,18 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env", override=True)
 
 pymysql.version_info = (2, 2, 1, 'final', 0)
 pymysql.install_as_MySQLdb()
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ti-prtjm(d_p7ve!r(g&4&(=+*_vn*x+*3z^ge567i72tr-5)1')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# FINAL UNBREAKABLE PATCH - Hardcoded Fallbacks
+SECRET_KEY = 'django-insecure-ti-prtjm(d_p7ve!r(g&4&(=+*_vn*x+*3z^ge567i72tr-5)1'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
+    'django_prometheus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -22,104 +24,69 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'corsheaders',
-    'rest_framework',
-    'pages',
-    'subway',
+    'subway',   
     'stories',
     'library',
+    'rest_framework',
+    'storages',
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("SB_DB_NAME", "postgres"),
-        "USER": os.getenv("SB_DB_USER", "postgres"),
-        "PASSWORD": (os.getenv("SB_DB_PASSWORD") or "hisubtory1234"),
-        "HOST": os.getenv("SB_DB_HOST", "db-postgres"),
+        "USER": os.getenv("SB_DB_USER", "postgres.jlvhwihvmivvihffhxse"),
+        "PASSWORD": "hisubtory1234",
+        "HOST": os.getenv("SB_DB_HOST", "aws-1-ap-northeast-2.pooler.supabase.com"),
         "PORT": os.getenv("SB_DB_PORT", "5432"),
     },
     "mysql": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME", "hisubtory_db"),
         "USER": os.getenv("DB_USER", "admin"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "mysql_password"),
-        "HOST": os.getenv("DB_HOST", "db-mysql"),
+        "PASSWORD": "8gEEJTwfFTMRhRFIMNrF",
+        "HOST": os.getenv("DB_HOST", "hisubtory-db.cnwkq8oe8jr5.ap-northeast-2.rds.amazonaws.com"),
         "PORT": os.getenv("DB_PORT", "3306"),
     }
 }
 
 DATABASE_ROUTERS = ['project.router.DatabaseRouter']
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
+REDIS_HOST = os.getenv("REDIS_HOST", "hisubtory-redis.rkthpz.0001.apn2.cache.amazonaws.com")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"}
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 LANGUAGE_CODE = 'ko-kr'
 TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 USE_TZ = True
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
-
-# --- 핵심 세션 공유 설정 ---
-SESSION_COOKIE_NAME = 'hisubtory_sessionid'
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
-
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = os.getenv("REDIS_PORT", "6379")
-REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": REDIS_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
@@ -127,6 +94,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://hisubtory-alb-1990322498.ap-northeast-2.elb.amazonaws.com",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://hisubtory-alb-1990322498.ap-northeast-2.elb.amazonaws.com",
-]
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
